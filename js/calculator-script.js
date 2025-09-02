c428f3843df56bf1d6a66faf822672efe519aca0
timocheu <ccs.timotheoche@gmail.com>
2025-09-02 18:24:59 +0800

    refactor: beautify

--- js/calculator-script.js
+++ js/calculator-script.js
@@ -1,27 +1,34 @@
-const num1 = document.getElementById("txtNum1");
-const num2 = document.getElementById("txtNum2");
 
 function Calculate(event)
 {
+    const num1 = document.getElementById("txtNum1");
+    const num2 = document.getElementById("txtNum2");
+
+    if (event.target.value === "CLEAR") {
+        num1.value = '';
+        num2.value = '';
+        return;
+    }
+
     let n1 = +num1.value;
     let n2 = +num2.value;
+
+    let res = "Result: ";
     
     switch (event.target.value) {
         case '+':
-            alert(n1+n2);
+            res += n1+n2;
             break;
         case '-':
-            alert(n1-n2);
+            res += n1-n2;
             break;
         case '*':
-            alert(n1*n2);
+            res += n1*n2;
             break;
         case '/':
-            alert(n1/n2);
-            break;
-        default:
-            num1.value = '';
-            num2.value = '';
+            res += n1/n2;
             break;
     }
+
+    alert(res);
 }
